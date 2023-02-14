517e51fe2091f5a6fae81bc61292eb45adb2f928
Jerin A Mathews <jerinamathews@gmail.com>
2023-02-15 00:51:35 +0530

    fix

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerActivity.kt
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerActivity.kt
@@ -158,17 +158,10 @@ class ImagePickerActivity : AppCompatActivity(), ImagePickerInteractionListener 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         actionBar = supportActionBar
-        val backResourceId: Int = if (resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
-            // For right-to-left layouts, pick the drawable that points to the right (forward).
-            R.drawable.ef_ic_arrow_forward
-        } else {
-            // For left-to-right layouts, pick the drawable that points to the left (back).
-            R.drawable.ef_ic_arrow_back
-        }
 
         actionBar?.run {
             setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(backResourceId)
+//            setHomeAsUpIndicator(backResourceId)
             setDisplayShowTitleEnabled(true)
         }
     }
